f13bba7ef6e883e88c57bdba3d8a0ed5208f7d7f
storezhang <storezhang@gmail.com>
2025-05-07 18:28:16 +0800

    fix(数据库): 修复已存在的迁移，当Version变大时还在插入数据导致的事务回退

--- internal/internal/db/upgrade.go
+++ internal/internal/db/upgrade.go
@@ -37,27 +37,27 @@ func (u *Upgrade) Exec(ctx context.Context, migration kernel.Migration) (err err
 	if exists, ge := u.migration.Get(ctx, entity); nil != ge {
 		err = ge
 	} else if !exists || version > entity.Version {
-		err = u.migration.Tx(u.exec(ctx, migration, version))
+		err = u.migration.Tx(u.exec(ctx, migration, version, exists))
 	}
 
 	return
 }
 
-func (u *Upgrade) exec(ctx context.Context, migration kernel.Migration, version uint16) func(*sql.Tx) error {
+func (u *Upgrade) exec(ctx context.Context, migration kernel.Migration, version uint16, exists bool) func(*sql.Tx) error { // nolint:lll
 	return func(tx *sql.Tx) (err error) {
 		now := time.Now()
 		if ue := migration.Upgrade(ctx); nil != ue {
 			err = ue
-		} else if affected, ie := u.insert(ctx, tx, &model.Migration{
+		} else if affected, se := u.save(ctx, tx, &model.Migration{
 			Id:          migration.Id(),
 			Version:     version,
 			Description: migration.Description(),
 			Created:     now,
 			Updated:     now,
-		}); nil != ie {
-			err = ie
+		}, exists); nil != se {
+			err = se
 		} else if 0 == affected {
-			err = exception.New().Message("未插入任何数据").Build()
+			err = exception.New().Message("未保存任何数据").Build()
 		}
 
 		// 检查执行是否有误，如果出错执行降级操作
@@ -69,6 +69,48 @@ func (u *Upgrade) exec(ctx context.Context, migration kernel.Migration, version 
 
 		return
 	}
+}
+
+func (u *Upgrade) save(ctx context.Context, tx *sql.Tx, migration *model.Migration, exists bool) (affected int64, err error) { // nolint:lll
+	if exists {
+		affected, err = u.update(ctx, tx, migration)
+	} else {
+		affected, err = u.insert(ctx, tx, migration)
+	}
+
+	return
+}
+
+func (u *Upgrade) update(ctx context.Context, tx *sql.Tx, migration *model.Migration) (affected int64, err error) {
+	if query, ise := u.updateSQL(); nil != ise {
+		err = ise
+	} else if result, ece := tx.ExecContext(
+		ctx, query,
+		migration.Version, time.Now(), migration.Description, migration.Id,
+	); nil != ece {
+		err = ece
+	} else if nil != result {
+		affected, err = result.RowsAffected()
+	}
+
+	return
+}
+
+func (u *Upgrade) updateSQL() (sql string, err error) {
+	switch u.dt {
+	case db.TypeMySQL:
+		sql = fmt.Sprintf(`UPDATE %s SET version = ?, updated = ?, description = ? WHERE id = ?`, u.config.Table)
+	case db.TypePostgres:
+		sql = fmt.Sprintf(`UPDATE %s SET version = $1, updated = $2, description = $3 WHERE id = $4`, u.config.Table)
+	case db.TypeSQLite:
+		sql = fmt.Sprintf(`UPDATE %s SET version = ?, updated = ?, description = ? WHERE id = ?`, u.config.Table)
+	case db.TypeOracle:
+		sql = fmt.Sprintf(`UPDATE %s SET version = $1, updated = $2, description = $3 WHERE id = $4`, u.config.Table)
+	default:
+		err = exception.New().Message("不被支持的数据库类型").Field(field.New("type", u.dt)).Build()
+	}
+
+	return
 }
 
 func (u *Upgrade) insert(ctx context.Context, tx *sql.Tx, migration *model.Migration) (affected int64, err error) {
